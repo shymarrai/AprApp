135709cb4c721f8c36af475eca6261e303b4b7ec
Bruno Gomes <icestonebruno@gmail.com>
2021-09-30 08:47:28 -0300

    a

--- App.js
+++ App.js
@@ -2,10 +2,23 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+import ImageLogo from "./assets/logoEllca.svg"
+import { useFonts, Inter_700Bold } from '@expo-google-fonts/inter';
+
+
 export default function App() {
+  let [fontsLoaded] = useFonts({
+    Inter_700Bold,
+  });
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={[{fontSize: 29,fontFamily: 'Inter_700Bold'}]}>APR VIRTUAL</Text>
+      <View style={styles.lineDivisor}></View>
+      <ImageLogo width={'80%'} height={'80%'} style={styles.imageLogo}/>
+      <View>
+
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -18,4 +31,14 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  textTitle:{
+    fontSize: 29,
+    fontWeight: 'normal',
+  },
+  lineDivisor:{
+    width: '75%',
+    height: 1,
+    backgroundColor: '#1295FA',
+    alignSelf: 'center'
+  }
 });
