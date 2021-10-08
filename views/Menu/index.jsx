cb7f9fef20d3dd3f3f53b074abff9dc2728fb247
Bruno Gomes <icestonebruno@gmail.com>
2021-10-08 14:50:14 -0300

    primary

--- views/Menu/index.jsx
+++ views/Menu/index.jsx
@@ -1,5 +1,7 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { View } from 'react-native';
+
+import { MotiView } from 'moti'
 
 import { HeaderSeparator } from '../../components/HeaderSeparator'
 import { Button } from '../../components/Button'
@@ -10,19 +12,36 @@ import {styles} from './styles'
 export function Menu({ navigation }) {
 
   return (
-    <View style={styles.container}>
+    <MotiView style={[styles.container]}
+      from={{ opacity:0 }}
+      animate={{ opacity:1 }}
+      transition={{
+        type: 'timing',
+        duration: 2000
+      }}
+    >
+
       <View style={[styles.box, {marginTop:'-15%'}]}>
         <HeaderSeparator content={'APR VIRTUAL'}/>
         
       </View>
-        <ImageLogo width={'250'} height={'250'} style={styles.imageLogo}/>     
-      <View style={styles.box}>
+
+      <ImageLogo width={'250'} height={'250'} style={styles.imageLogo}/>
+
+      <MotiView style={styles.box}
+        from={{left:'-100%'}}
+        animate={{left:'0%'}}
+        transition={{
+          type: 'timing',
+          duration:2000
+        }}
+      >
         <Button title={'GERAR APR DIGITAL'} onPress={() => navigation.navigate('AprSelector')}/>   
         <Button title={'GERAR APR DE RECUSA'} onPress={() => navigation.navigate('AprRecuse')} />
         <Button title={'AJUDA'} onPress={() => navigation.navigate('Help')}/>
-      </View>
+      </MotiView>
 
-  </View>
+  </MotiView>
   );
 }
 
